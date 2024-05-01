38d5bd6c0a922af887aa222422a8db974b7c822d
Nedal-Esrar Ahmad <113736086+Nedal-Esrar@users.noreply.github.com>
2024-05-01 14:29:48 +0300

    Time: 3 ms (35.11%), Space: 8 MB (37.28%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -3,15 +3,13 @@ public:
     vector<int> sumZero(int n) {
         vector<int> ret;
         
-        if (n % 2 == 1) {
-            ret.push_back(0);
-            
-            --n;
-        }
-        
         for (int i = 1; i <= n / 2; ++i) {
             ret.push_back(i);
             ret.push_back(-i);
+        }
+        
+        if (n % 2 == 1) {
+            ret.push_back(0);
         }
         
         return ret;
