a55b1a9c40a75c146eb236b82a868ea17c3cc929
Nedal-Esrar Ahmad <113736086+Nedal-Esrar@users.noreply.github.com>
2024-05-04 10:42:34 +0300

    Time: 0 ms (100.00%), Space: 7.1 MB (53.60%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int hamming_distance = 0;
-        
-        while (x != 0 || y != 0) {
-            hamming_distance += (x & 1) ^ (y & 1);
-            
-            x >>= 1;
-            y >>= 1;
-        }
-        
-        return hamming_distance;
+        return __builtin_popcount(x ^ y);
     }
 };
