51592a6f07882d31f50969a70b238450704e9dd1
Nedal-Esrar Ahmad <113736086+Nedal-Esrar@users.noreply.github.com>
2024-05-26 16:39:39 +0300

    Time: 6 ms (91.44%), Space: 9.3 MB (11.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        
+        for (char move : moves) {
+            switch (move) {
+                case 'U':
+                    --y;
+                    break;
+                    
+                case 'D':
+                    ++y;
+                    break;
+                    
+                case 'L':
+                    --x;
+                    break;
+                    
+                case 'R':
+                    ++x;
+                    break;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+};
